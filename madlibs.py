a0837cae4da0412eb32e975c2d19f2201d22f770
Lukasz <Lukasz310@gmail.com>
2023-02-28 11:20:10 +0100

    initial commit

---
+++ madlibs.py
@@ -0,0 +1,13 @@
+# string concatenation (aka how to put strings toghether)
+# supose we wont to create a string that says "subscribe to ______"
+# youtuber = ""
+
+# # a few ways to do this
+# print ("subscribe to " + youtuber)
+# print ("subscribe to {}".format(youtuber))
+# print (f"subscribe to {youtuber}")
+
+adj = ""
+
+madlib = f"Computer programming is so {adj}! It makes me so exited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
