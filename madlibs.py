14269c3a5dcd635174e65ec126b2ecc746ca3974
Lukasz Kamrowski <Lukasz310@gmail.com>
2023-03-01 10:41:00 +0100

    Test push from WSL

--- madlibs.py
+++ madlibs.py
@@ -7,7 +7,12 @@
 # print ("subscribe to {}".format(youtuber))
 # print (f"subscribe to {youtuber}")
 
-adj = ""
+adj = "ADJ"
+verb1 = "VERB1"
+verb2 = "VERB2"
+fameous_person = "FP"
 
 madlib = f"Computer programming is so {adj}! It makes me so exited all the time because \
-    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+    I love to {verb1}. Stay hydrated and {verb2} like you are {fameous_person}!"
+
+print(madlib)
