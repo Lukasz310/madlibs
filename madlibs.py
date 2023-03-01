ccc5526ff82414a2e98298af4f7dc21a49978989
Lukasz Kamrowski <Lukasz310@gmail.com>
2023-03-01 11:21:32 +0100

    program completed

--- madlibs.py
+++ madlibs.py
@@ -7,12 +7,12 @@
 # print ("subscribe to {}".format(youtuber))
 # print (f"subscribe to {youtuber}")
 
-adj = "ADJ"
-verb1 = "VERB1"
-verb2 = "VERB2"
-fameous_person = "FP"
+adj = input("Adjetive: ")
+verb1 = input("VERB1: ")
+verb2 = input("VERB2: ")
+fameous_person = input("Fameous Person: ")
 
 madlib = f"Computer programming is so {adj}! It makes me so exited all the time because \
-    I love to {verb1}. Stay hydrated and {verb2} like you are {fameous_person}!"
+I love to {verb1}. Stay hydrated and {verb2} like you are {fameous_person}!"
 
 print(madlib)
